6d8fade0b9a6d43cfd4d3444707a36922b2a6f3e
plummm <etenalcxz@gmail.com>
2022-09-29 17:45:18 -0700

    [script/ubuntu] disable ubuntu auto update

--- syzmorph/scripts/deploy-ubuntu-image.sh
+++ syzmorph/scripts/deploy-ubuntu-image.sh
@@ -87,6 +87,15 @@ WantedBy=multi-user.target
 EOF
 
     chmod 644 /etc/systemd/system/dhclient.service
+
+    # Disable auto update
+    rm /etc/apt/apt.conf.d/20auto-upgrades
+    cat << EOF > /etc/apt/apt.conf.d/20auto-upgrades
+APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Download-Upgradeable-Packages "0";
+APT::Periodic::AutocleanInterval "0";
+APT::Periodic::Unattended-Upgrade "0";
+EOF
     systemctl enable dhclient.service
 }
 
