23ba0dc71646b116de78ccdf740c29c060bbf660
plummm <etenalcxz@gmail.com>
2022-10-05 14:44:51 -0700

    [script] install trace-cmd and vim in separate apt

--- syzmorph/scripts/create-image.sh
+++ syzmorph/scripts/create-image.sh
@@ -179,7 +179,7 @@ if [ $PERF = "true" ]; then
     rm -r $DIR/tmp/$BASENAME
 fi
 
-sudo chroot $DIR /bin/bash -c "trace-cmd vim"
+sudo chroot $DIR /bin/bash -c "apt-get update; apt-get install -y trace-cmd vim"
 
 # Add udev rules for custom drivers.
 # Create a /dev/vim2m symlink for the device managed by the vim2m driver
