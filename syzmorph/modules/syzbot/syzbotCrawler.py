17ccca3de95d100a8141ef2781d5a64ed36a2f13
plummm <etenalcxz@gmail.com>
2022-11-28 13:09:10 -0800

    [module/syzbot] adopt android commit for syzbot

--- syzmorph/modules/syzbot/syzbotCrawler.py
+++ syzmorph/modules/syzbot/syzbotCrawler.py
@@ -691,8 +691,8 @@ class Crawler:
                             tags = case.find_all('td', {"class": "tag"})
                             m = re.search(r'id=([0-9a-z]*)', tags[0].next.attrs['href'])
                             if m is None:
-                                m = re.search(r'commits\/([0-9a-z]*)', tags[0].next.attrs['href'])
-                            commit = m.groups()[0]
+                                m = re.search(r'(commits\/|\+log\/)([0-9a-z]*)', tags[0].next.attrs['href'])
+                            commit = m.groups()[1]
                         except Exception as e:
                             self.logger.exception("Failed to retrieve case \"commit\" {}{}{}: {}".format(syzbot_host_url, syzbot_bug_base_url, hash, e))
                             continue
